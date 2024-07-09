fc12c74bffd59c91acd1a3de26f2050010f31148
Arkadiusz Korszun <arkadiusz.korszun.programista@gmail.com>
2024-07-09 21:55:25 +0200

    Added addressee class and create files for features

---
+++ AdresatMenager.h
@@ -0,0 +1,18 @@
+#ifndef ADRESATMENAGER_H
+#define ADRESATMENAGER_H
+#include <iostream>
+#include <vector>
+#include <windows.h>
+#include <fstream>
+#include <sstream>
+#include "Adresat.h"
+#include "PlikZAdresatami.h"
+
+using namespace std;
+
+class AdresatMenager
+{
+
+};
+
+#endif // ADRESATMENAGER_H
